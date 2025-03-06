88ef57b67efdfd39dd08bc15d2d6e57b078a201b
RobinEatingWorm <gavinpu27@gmail.com>
2025-03-06 06:34:17 -0500

    OS-specific run scripts

--- run_linux.sh
+++ run_linux.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export JAVA_HOME=./lib/jdk-21.0.5.jdk/Contents/Home
-PATH_TO_FX=./lib/javafx-sdk-23.0.1/lib
+JDK_DIR=
+JAVAFX_DIR=
 
-echo Compiling...
+JAVA_HOME=lib/$JDK_DIR
+PATH_TO_FX=lib/$JAVAFX_DIR/lib
+
 javac --module-path $PATH_TO_FX --add-modules javafx.controls -d out $(find src -name "*.java")
-echo Running...
 java --module-path $PATH_TO_FX --add-modules javafx.controls --class-path out spidersolitaire.App
