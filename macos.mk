be898a9113d85e7053a97a1874fb7f741ee94983
RobinEatingWorm <gavinpu27@gmail.com>
2025-09-17 20:10:22 -0400

    Package build for macOS

---
+++ macos.mk
@@ -0,0 +1,9 @@
+# Operating system
+OS = macOS
+
+# Library directories
+JDK_DIR = jdk-21.0.5.jdk
+FX_SDK_DIR = javafx-sdk-23.0.1
+FX_JMODS_DIR = javafx-jmods-23.0.1
+
+include make.mk
---
+++ make.mk
@@ -0,0 +1,72 @@
+# Directories
+SRC_DIR = src
+LIB_DIR = lib
+OUT_DIR = out
+BUILD_DIR = build
+RUNTIME_DIR = runtime
+
+# Java tools
+JAR = $(JAVA_HOME)/bin/jar
+JAVA = $(JAVA_HOME)/bin/java
+JAVAC = $(JAVA_HOME)/bin/javac
+JLINK = $(JAVA_HOME)/bin/jlink
+JPACKAGE = $(JAVA_HOME)/bin/jpackage
+
+# Paths to JavaFX SDK and jmods
+FX_SDK_PATH = $(LIB_DIR)/$(FX_SDK_DIR)/lib
+FX_JMODS_PATH = $(LIB_DIR)/$(FX_JMODS_DIR)
+
+# Modules to add
+JAVA_MODULES = java.base
+FX_MODULES = javafx.base,javafx.controls,javafx.graphics
+
+# Targets
+TARGET = spidersolitaire
+TARGET_JAR = $(TARGET).jar
+TARGET_CLASS = $(TARGET).App
+
+# Java source files
+SOURCES = $(shell find $(SRC_DIR) -name "*.java")
+
+# Java manifest file
+MANIFEST = META-INF/MANIFEST.MF
+
+# OS-specific variables
+ifeq ($(OS), Windows)
+# TODO
+else ifeq ($(OS), macOS)
+JAVA_HOME = $(LIB_DIR)/$(JDK_DIR)/Contents/Home
+TARGET_APP = $(TARGET).app
+TARGET_TYPE = app-image
+else ifeq ($(OS), Linux)
+# TODO
+endif
+
+# Main rule
+all: $(TARGET_APP)
+
+# Application package
+$(TARGET_APP): $(BUILD_DIR)/$(TARGET_JAR) $(RUNTIME_DIR)
+	$(JPACKAGE) --name $(TARGET) --input $(BUILD_DIR) --main-jar $(TARGET_JAR) --main-class $(TARGET_CLASS) -t $(TARGET_TYPE) --runtime-image $(RUNTIME_DIR)
+
+# Runtime image
+$(RUNTIME_DIR):
+	$(JLINK) --output $@ --module-path $(JAVA_HOME)/jmods:$(FX_JMODS_PATH) --add-modules $(JAVA_MODULES),$(FX_MODULES)
+
+# JAR archive
+$(BUILD_DIR)/$(TARGET_JAR): $(OUT_DIR) $(MANIFEST)
+	$(JAR) -c -f $@ -m $(MANIFEST) -C $(OUT_DIR) .
+
+# Compilation to class files
+$(OUT_DIR): $(SOURCES)
+	$(JAVAC) --module-path $(FX_SDK_PATH) --add-modules $(FX_MODULES) -d $@ $^
+
+# Test without full build
+test: $(OUT_DIR)
+	$(JAVA) --module-path $(FX_SDK_PATH) --add-modules $(FX_MODULES) --class-path $< $(TARGET_CLASS)
+
+# Remove build artifacts
+clean:
+	rm -r -f $(OUT_DIR) $(BUILD_DIR) $(RUNTIME_DIR) $(TARGET_APP)
+
+.PHONY: all test clean
